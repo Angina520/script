056e78306974967a13d2186a20324f640b77dc2f
ind W <318161934@qq.com>
2021-03-13 07:56:18 +0800

    final

--- Course select.go
+++ Course select.go
@@ -34,20 +34,34 @@ func httpDo(url, cookie string) error {
 }
 
 func main() {
-	var url, cookie string
+	var n interface{}
+	fmt.Print("请先确定想选择几门课（只能为正整数）：")
 	std := bufio.NewScanner(os.Stdin)
-	fmt.Print("请输入url：")
 	std.Scan()
-	url = std.Text()
-	fmt.Print("请输入cookie：")
-	std.Scan()
-	cookie = std.Text()
+	n = std.Text()
+	value, ok := n.(int)
+	if !ok {
+		fmt.Println("类型错误")
+		time.Sleep(time.Second * 3)
+		return
+	}
+	url, cookie := make([]string, value), make([]string, value)
+	for i := 0; i < value; i++ {
+		fmt.Print("请输入url：")
+		std.Scan()
+		url[i] = std.Text()
+		fmt.Print("请输入cookie：")
+		std.Scan()
+		cookie[i] = std.Text()
+	}
 	for {
-		err := httpDo(url, cookie)
-		if err != nil {
-			fmt.Println("错误，终止运行!请检查url和cookie")
-			time.Sleep(3 * time.Second)
-			break
+		for i := 0; i < value; i++ {
+			err := httpDo(url[i], cookie[i])
+			if err != nil {
+				fmt.Println("错误，终止运行!请检查url和cookie")
+				time.Sleep(3 * time.Second)
+				return
+			}
 		}
 		time.Sleep(3 * time.Second)
 	}
